86f7a21cda824b65274d5305565ed633c15af2ed
sidharthify <wednisegit@gmail.com>
2025-12-04 11:51:14 +0530

    breathe: Import icon
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sidharthify.breathe"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
